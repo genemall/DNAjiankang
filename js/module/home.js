10c503a1ab9e484038dbabba0a9d27116c0dd99d
shileiding <shileiding@163.com>
2016-12-03 20:38:49 +0800

    实现主页幻灯片的播放，优化代码

--- js/module/home.js
+++ js/module/home.js
@@ -2,45 +2,50 @@
  * home page
  */
 
-angular.module('home', ['ui.router'])
-    .config(['$stateProvider',
-        function ($stateProvider) {
-            $stateProvider
-                .state("home", {
-                    url: '/home',
-                    templateUrl: 'view/home.html',
-                    resolve: {
-                    	classifyResolve: function (httpService) {
-                            return httpService.get('json/fenlei.json')
-                             .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-                                    return data.classifies;
-                                });;
-                        },
-                        
-                        /*
-                        productsResolve: function (httpService) {
-                            return httpService.get('json/products.json');
-                        },
-                        productObj: function (httpService, util,$state) {
-                            return httpService.get('json/products.json')
-                                .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-                                    return util.queryItemById(data.products, 1);
-                                });
-                        },
-                        */
+//angular.module('home', ['ui.router'])
+var homeModule = angular.module('home', ['ui.router']);
+sliderTest(homeModule);
+homeModule.config(['$stateProvider',
+    function ($stateProvider) {
+        $stateProvider
+            .state("home", {
+                url: '/home',
+                templateUrl: 'view/home.html',
+                resolve: {
+                	classifyResolve: function (httpService) {
+                        return httpService.get('json/fenlei.json')
+                         .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
+                                return data.classifies;
+                            });;
                     },
-                     controller: function ($scope,$stateParams,$rootScope,classifyResolve) {
-                        $scope.classifies=classifyResolve
-                        console.log($scope.classifies)
-                        $scope.search='搜索'
-                     }
+                    
+                    /*
+                    productsResolve: function (httpService) {
+                        return httpService.get('json/products.json');
+                    },
+                    productObj: function (httpService, util,$state) {
+                        return httpService.get('json/products.json')
+                            .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
+                                return util.queryItemById(data.products, 1);
+                            });
+                    },
+                    */
+                },
+                 controller: function ($scope,$stateParams,$rootScope,classifyResolve) {
+                    $scope.classifies=classifyResolve
+                    console.log($scope.classifies)
+                    $scope.search='搜索'
+                    $scope.data = [{img: "./resource/img/5.jpg",link:'#'},
+			                     {img: "./resource/img/2.jpg",link:'#'},
+			                     {img: "./resource/img/3.jpg",link:'#'}];
+                 }
 
-                })
-                .state("error404",{
-                    url:'/error404',
-                    templateUrl: 'view/404.html',
-                    controller:function($scope,$stateParams){
-                    }
-                })
-        }
-    ])
+            })
+            .state("error404",{
+                url:'/error404',
+                templateUrl: 'view/404.html',
+                controller:function($scope,$stateParams){
+                }
+            })
+    }
+])
