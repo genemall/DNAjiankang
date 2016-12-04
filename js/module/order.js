f3fce10e749df14887458404418bbe000320ee9f
lsabella <duanhuanlsabella@126.com>
2016-12-04 21:00:58 +0800

    弹框样式补，order单数据显示初步实现

--- js/module/order.js
+++ js/module/order.js
@@ -9,9 +9,20 @@ angular.module('order', ['ui.router'])
                 .state("order", {
                     url: '/order',
                     templateUrl: 'view/order.html',
-                    controller: function($scope,cartService){
+                     resolve: {
+                	classifyResolve: function (httpService) {
+                        return httpService.get('json/order.json')
+                         .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
+                                return data.products;
+                            });;
+                   		},
+               		 },
+                 
+                    
+                    controller: function($scope,cartService,classifyResolve){
                         //调用服务获取我的购物车对象
                         $scope.cart = cartService.myCart;
+                        $scope.orderlist=classifyResolve;
                     },
                     
                     //配置导航
