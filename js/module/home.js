a11991809f3bbb7811b7b881af1539c56116558e
15258230407@163.com <shileiding@pptv.com>
2016-12-06 12:04:58 +0800

    搜索功能实现，未实现浮沉列表

--- js/module/home.js
+++ js/module/home.js
@@ -18,7 +18,7 @@ homeModule.config(['$stateProvider',
                                 return data.classifies;
                             });;
                     },
-                    
+
                     /*
                     productsResolve: function (httpService) {
                         return httpService.get('json/products.json');
@@ -34,10 +34,38 @@ homeModule.config(['$stateProvider',
                  controller: function ($scope,$stateParams,$rootScope,classifyResolve) {
                     $scope.classifies=classifyResolve
                     console.log($scope.classifies)
-                    $scope.search='搜索'
                     $scope.data = [{img: "./resource/img/5.jpg",link:'#'},
 			                     {img: "./resource/img/2.jpg",link:'#'},
 			                     {img: "./resource/img/3.jpg",link:'#'}];
+
+			        $scope.focusing = ''; //获得焦点时增加样式weui_search_focusing
+                    $scope.isSearchShow = false; //是否显示搜索框的下拉列表
+                    $scope.searchInput = ''; //搜索内容清空
+                    $scope.searchText = true; //是否显示默认状态下的搜索条样式
+
+                    $scope.searchClickEvent = function() {
+                        $scope.searchText = false;
+                        document.getElementById('searchInput').focus();
+                    }
+
+                    $scope.searchFocusEvent = function() {
+                        $scope.focusing = 'weui-search-bar_focusing';
+                    }
+
+                    $scope.searchBlurEvent = function() {
+                        $scope.searchText = true;
+                        $scope.isSearchShow = false;
+                        $scope.focusing = '';
+                        $scope.searchInput = '';
+                    }
+
+                    $scope.searchKeyupEvent = function() {
+                        if ($scope.searchInput) {
+                            $scope.isSearchShow = true;
+                        } else {
+                            $scope.isSearchShow = false;
+                        }
+                    }
                  }
 
             })
