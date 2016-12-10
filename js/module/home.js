d0b86b3c2917251bae90e8196f5932be746c2d8e
shileiding <shileiding@163.com>
2016-12-10 23:32:19 +0800

    主页和商品列表的实现注释

--- js/module/home.js
+++ js/module/home.js
@@ -11,11 +11,11 @@ homeModule.config(['$stateProvider',
             .state("home", {
                 url: '/home',
                 templateUrl: 'view/home.html',
-                resolve: {
-                	classifyResolve: function (httpService,$rootScope) {
-                        return httpService.get($rootScope.baseURL+'classify/phoneclsall.do')
+                resolve: { //预加载的功能，在页面渲染出现之前，提前加载这些数据，并在controller中引用
+                	classifyResolve: function (httpService,$rootScope) { //定义预加载的函数
+                        return httpService.get($rootScope.baseURL+'classify/phoneclsall.do') //通过Service获取接口对应的json数据
                          .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-                                return data.classifies;
+                                return data.classifies; //对返回的数据进行处理
                             });;
                     },
                     /*
@@ -28,15 +28,17 @@ homeModule.config(['$stateProvider',
                     */
                 },
                  controller: function ($scope,$stateParams,$rootScope,classifyResolve) {
-                    $scope.classifies=classifyResolve
+                    $scope.classifies=classifyResolve //双向绑定 数据和前段的标签，此处为 商品分类的循环
+                    
+                    //以下是幻灯片展示部分，可以不看
                     $scope.data = [{img: "./resource/img/5.jpg",link:'#'},
 			                     {img: "./resource/img/2.jpg",link:'#'},
 			                     {img: "./resource/img/3.jpg",link:'#'}];
+			        //以下是搜索部分，可不看
 			        $scope.focusing = ''; //获得焦点时增加样式weui_search_focusing
                     $scope.isSearchShow = false; //是否显示搜索框的下拉列表
                     $scope.searchInput = ''; //搜索内容清空
                     $scope.searchText = true; //是否显示默认状态下的搜索条样式
-
                     $scope.searchClickEvent = function() {
                         $scope.searchText = false;
                         document.getElementById('searchInput').focus();
--- js/module/product.js
+++ js/module/product.js
@@ -15,9 +15,9 @@ angular.module('product', ['ui.router'])
 	                    },
 	                },
                     controller: function($scope,httpService,product_list,$rootScope){
-                    	$scope.products=product_list
-                    	$scope.products_show=product_list[0].listProduct
-                    	$scope.getProductsData=function(id){
+                    	$scope.products=product_list //导航页的三个标签，对应的展示
+                    	$scope.products_show=product_list[0].listProduct//每个标签对应的商品信息，默认是第一个标签
+                    	$scope.getProductsData=function(id){ //点击其他标签，重新获取数据，并在最后的返回中给予赋值
                     		httpService.get($rootScope.baseURL+'product/phoneproall.do?clsId='+id)
 	                         .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
 	                                $scope.products_show=data;
