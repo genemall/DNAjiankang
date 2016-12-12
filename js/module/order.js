d90f4c2c26c893fb22c092f5b92445f6f1ea92fb
15258230407@163.com <shileiding@pptv.com>
2016-12-12 19:40:03 +0800

    实现 order 的数据展现

--- js/module/order.js
+++ js/module/order.js
@@ -19,9 +19,18 @@ angular.module('order', ['ui.router'])
                		 },
 
 
-                    controller: function($scope,httpService,$stateParams,order_show){
+                    controller: function($scope,httpService,$rootScope,$stateParams,order_show){
                         $scope.order_show=order_show;
                         $scope.ord_state=$stateParams.ord_state
+                        $scope.is_select_id=1
+                        $scope.getOrdersData = function(ord_state){
+                            httpService.get($rootScope.baseURL+'/order/phoneGetOrdersByUserId.do?userId='+$stateParams.id+"&ordState="+ord_state)
+                             .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
+                                    $scope.order_show=data;
+                                    $scope.is_select_id=ord_state
+                                    $scope.ord_state=ord_state
+                            });
+                        }
                     },
 
                     //配置导航
