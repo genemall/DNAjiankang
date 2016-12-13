9fbc8cc0e3fc089d2f3a78422ba0ab017de368f8
15258230407@163.com <shileiding@pptv.com>
2016-12-13 20:56:48 +0800

    增加 cart.js 的watch处理

--- js/module/cart.js
+++ js/module/cart.js
@@ -13,27 +13,30 @@ angular.module('cart', ['ui.router','cartMd'])
 	                	cart_list: function (httpService,$rootScope,$stateParams) {
 	                        return httpService.get($rootScope.baseURL+'cart/phonecartsel.do?userId='+$stateParams.id)
 	                         .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-	                                return data;
-	                            });
+	                             cart_datas=data.listOrderProduct;
+	                             for (var i = 0; i < $scope.cart_datas.length; i++) {
+                                        var item = $scope.cart_datas[i];
+                                        $scope.cart_total += item.proCount * item.product.proRateprice;
+                                 }
+	                             return data;
+	                        });
 	                    },
 	                },
                     controller:function($scope,cart_list,$rootScope){
                     	$scope.cart_datas=cart_list.listOrderProduct;
-                        var getCartTotal = function () { //获取购物车价格
-                    	    $scope.cart_total = 0;
+                    	$scope.$watch("cart_datas", function() {//监控数据变化
+                            $scope.cart_total = 0;
                             for (var i = 0; i < $scope.cart_datas.length; i++) {
                                 var item = $scope.cart_datas[i];
                                 $scope.cart_total += item.proCount * item.product.proRateprice;
                             }
-                        }
-                    	getCartTotal()
+                        }, true);
                         $scope.update_cart_pro_count = function(proId,count) {
     		               for (var i = 0; i < $scope.cart_datas.length; i++) {
                                 if ($scope.cart_datas[i].proId == proId) {
                                     $scope.cart_datas[i].proCount = $scope.cart_datas[i].proCount + count;//这里可以增加上下限制
                                 }
                             }
-    		               getCartTotal()
     		            }
 
                     },
