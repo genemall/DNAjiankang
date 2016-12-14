1efb0c9234abea3bf79da344a1627401e5e96c1c
15258230407@163.com <shileiding@pptv.com>
2016-12-14 11:03:59 +0800

    完成购物车的逻辑

--- js/module/cart.js
+++ js/module/cart.js
@@ -13,28 +13,54 @@ angular.module('cart', ['ui.router','cartMd'])
 	                	cart_list: function (httpService,$rootScope,$stateParams) {
 	                        return httpService.get($rootScope.baseURL+'cart/phonecartsel.do?userId='+$stateParams.id)
 	                         .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-	                             return data;
+	                             data_list=data.listOrderProduct;
+	                             for (var i = 0; i < data_list.length; i++) {
+	                                 data_list[i]['check'] = false
+	                                 data_list[i]['totalPrice']=data_list[i].proCount * data_list[i].product.proRateprice;
+	                             }
+	                             return data_list;
 	                        });
 	                    },
 	                },
                     controller:function($scope,cart_list,$rootScope){
                     	//http://blog.csdn.net/liaodehong/article/details/52493779
-                    	$scope.cart_datas=cart_list.listOrderProduct;
+                    	$scope.cart_datas=cart_list;
                     	$scope.$watch("cart_datas", function() {//监控数据变化
                             $scope.cart_total = 0;
+                            count=0
                             for (var i = 0; i < $scope.cart_datas.length; i++) {
-                                var item = $scope.cart_datas[i];
-                                $scope.cart_total += item.proCount * item.product.proRateprice;
+                                if($scope.cart_datas[i].check){
+                                    $scope.cart_total += $scope.cart_datas[i].totalPrice;
+                                    count++;
+                                }
                             }
+                            $scope.all=(count==$scope.cart_datas.length);
                         }, true);
                         $scope.update_cart_pro_count = function(proId,count) {
     		               for (var i = 0; i < $scope.cart_datas.length; i++) {
                                 if ($scope.cart_datas[i].proId == proId) {
-                                    $scope.cart_datas[i].proCount = $scope.cart_datas[i].proCount + count;//这里可以增加上下限制
+                                    $scope.cart_datas[i].proCount = $scope.cart_datas[i].proCount + count;
+                                    $scope.cart_datas[i].totalPrice = $scope.cart_datas[i].product.proRateprice*$scope.cart_datas[i].proCount;
                                 }
                             }
     		            }
 
+                        $scope.checkAll=function(){
+                            for(var i in $scope.cart_datas){
+                                 $scope.cart_datas[i].check=$scope.all;
+                            }
+                        }
+                        $scope.deleteSelect=function(){
+                            //发送httpServer 的post请求
+                            for(var i in $scope.cart_datas){
+                                if($scope.cart_datas[i].check){
+                                    $scope.cart_datas.splice(i,1);
+                                }
+                            }
+                        }
+                        $scope.createOrder=function(){
+                            //此次按照check选中的商品生成http post请求，并$location跳转到 订单详情界面
+                        }
                     },
                     //配置导航，回到父层
                     ncyBreadcrumb:{
