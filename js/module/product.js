293495df1d20705b01c81070ba9d3a80f30a4972
shileiding <shileiding@163.com>
2016-12-20 00:25:53 +0800

    jquery 实现滑动导航栏

--- js/module/product.js
+++ js/module/product.js
@@ -29,96 +29,61 @@ angular.module('product', ['ui.router'])
 	                                $scope.is_select_id = id
 	                       	});
                     	}
+                    	//jquery 实现 滑动导航栏
+					    $(".find_nav_list li").each(function(){
+					            $(".sideline").css({left:0});
+					            $(".find_nav_list li").eq(0).addClass("find_nav_cur").siblings().removeClass("find_nav_cur");
+					    });
+					    var nav_w=$(".find_nav_list li").first().width();
+					    $(".sideline").width(nav_w);
+					    $(".find_nav_list li").on('click', function(){
+					        nav_w=$(this).width();
+					        $(".sideline").stop(true);
+					        $(".sideline").animate({left:$(this).position().left},300);
+					        $(".sideline").width(nav_w);
+					        $(this).addClass("find_nav_cur").siblings().removeClass("find_nav_cur");
+					        var fn_w = ($(".find_nav").width() - nav_w) / 2;
+					        var fnl_l;
+					        var fnl_x = parseInt($(this).position().left);
+					        if (fnl_x <= fn_w) {
+					            fnl_l = 0;
+					        } else if (fn_w - fnl_x <= flb_w - fl_w) {
+					            fnl_l = flb_w - fl_w;
+					        } else {
+					            fnl_l = fn_w - fnl_x;
+					        }
+					        $(".find_nav_list").animate({
+					            "left" : fnl_l
+					        }, 300);
+					        var c_nav=$(this).find("a").text();
+					    });
+					    var fl_w=$(".find_nav_list").width();
+					    var flb_w=$(".find_nav_left").width();
+					    $(".find_nav_list").on('touchstart', function (e) {
+					        var touch1 = e.originalEvent.targetTouches[0];
+					        x1 = touch1.pageX;
+					        y1 = touch1.pageY;
+					        ty_left = parseInt($(this).css("left"));
+					    });
+					    $(".find_nav_list").on('touchmove', function (e) {
+					        var touch2 = e.originalEvent.targetTouches[0];
+					        var x2 = touch2.pageX;
+					        var y2 = touch2.pageY;
+					        if(ty_left + x2 - x1>=0){
+					            $(this).css("left", 0);
+					        }else if(ty_left + x2 - x1<=flb_w-fl_w){
+					            $(this).css("left", flb_w-fl_w);
+					        }else{
+					            $(this).css("left", ty_left + x2 - x1);
+					        }
+					        if(Math.abs(y2-y1)>0){
+					            e.preventDefault();
+					        }
+					    });
+
                     },
                 })
         }
     ])
 
 
-
-$(function(){
-    $(".find_nav_list").css("left",sessionStorage.left+"px");
-    $(".find_nav_list li").each(function(){
-        if($(this).find("a").text()==sessionStorage.pagecount){
-            $(".sideline").css({left:$(this).position().left});
-            $(".sideline").css({width:$(this).outerWidth()});
-            $(this).addClass("find_nav_cur").siblings().removeClass("find_nav_cur");
-            navName(sessionStorage.pagecount);
-            return false
-        }
-        else{
-            $(".sideline").css({left:0});
-            $(".find_nav_list li").eq(0).addClass("find_nav_cur").siblings().removeClass("find_nav_cur");
-        }
-    });
-    var nav_w=$(".find_nav_list li").first().width();
-    $(".sideline").width(nav_w);
-    $(".find_nav_list li").on('click', function(){
-        nav_w=$(this).width();
-        $(".sideline").stop(true);
-        $(".sideline").animate({left:$(this).position().left},300);
-        $(".sideline").animate({width:nav_w});
-        $(this).addClass("find_nav_cur").siblings().removeClass("find_nav_cur");
-        var fn_w = ($(".find_nav").width() - nav_w) / 2;
-        var fnl_l;
-        var fnl_x = parseInt($(this).position().left);
-        if (fnl_x <= fn_w) {
-            fnl_l = 0;
-        } else if (fn_w - fnl_x <= flb_w - fl_w) {
-            fnl_l = flb_w - fl_w;
-        } else {
-            fnl_l = fn_w - fnl_x;
-        }
-        $(".find_nav_list").animate({
-            "left" : fnl_l
-        }, 300);
-        sessionStorage.left=fnl_l;
-        var c_nav=$(this).find("a").text();
-        navName(c_nav);
-    });
-    var fl_w=$(".find_nav_list").width();
-    var flb_w=$(".find_nav_left").width();
-    $(".find_nav_list").on('touchstart', function (e) {
-        var touch1 = e.originalEvent.targetTouches[0];
-        x1 = touch1.pageX;
-        y1 = touch1.pageY;
-        ty_left = parseInt($(this).css("left"));
-    });
-    $(".find_nav_list").on('touchmove', function (e) {
-        var touch2 = e.originalEvent.targetTouches[0];
-        var x2 = touch2.pageX;
-        var y2 = touch2.pageY;
-        if(ty_left + x2 - x1>=0){
-            $(this).css("left", 0);
-        }else if(ty_left + x2 - x1<=flb_w-fl_w){
-            $(this).css("left", flb_w-fl_w);
-        }else{
-            $(this).css("left", ty_left + x2 - x1);
-        }
-        if(Math.abs(y2-y1)>0){
-            e.preventDefault();
-        }
-    });
-});
-function navName(c_nav) {
-    switch (c_nav) {
-        case "儿童基因":
-            sessionStorage.pagecount = "儿童基因";
-            break;
-        case "记忆基因":
-            sessionStorage.pagecount = "记忆基因";
-            break;
-        case "营养基因":
-            sessionStorage.pagecount = "营养基因";
-            break;
-        case "运动基因":
-            sessionStorage.pagecount = "运动基因";
-            break;
-        case "外貌基因":
-            sessionStorage.pagecount = "外貌基因";
-            break;
-        case "天赋基因":
-            sessionStorage.pagecount = "天赋基因";
-            break;
-    }
-}
