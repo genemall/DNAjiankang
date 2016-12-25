a2cb02dfbd1d7ef87d5107610fe066b4275ce561
shileiding <shileiding@163.com>
2016-12-25 17:45:03 +0800

    测试 微信支付

--- js/module/orderPay.js
+++ js/module/orderPay.js
@@ -20,18 +20,17 @@ angular.module('orderPay', ['ui.router'])
                     	$scope.orderDetail_datas=order_detail.mapOrderProductList;
                     	$scope.ordPrice=order_detail.ordPrice;
 						var post_data={'openId':'ofzXwvnbUQYrVMmYn8uxZuHbbX5g','finalmoney':order_detail.ordPrice,'orderId':order_detail.id}
-						console.log(post_data)
+						var pay_data={}
                     	httpService.post($rootScope.baseURL+'weixin/topay.do',post_data)
                              .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-                                console.log(data)
+                                pay_data=data
                         });
                     	$scope.gotoPay=function(){
-                			 WeixinJSBridge.invoke('getBrandWCPayRequest',{
-					  		 "appId" : "<%=appId%>","timeStamp" : "<%=timeStamp%>", "nonceStr" : "<%=nonceStr%>", "package" : "<%=packageValue%>","signType" : "MD5", "paySign" : "<%=paySign%>" 
-					   			},function(res){
+                			 WeixinJSBridge.invoke('getBrandWCPayRequest',pay_data,function(res){
 									WeixinJSBridge.log(res.err_msg);
 					// 				alert(res.err_code + res.err_desc + res.err_msg);
 						            if(res.err_msg == "get_brand_wcpay_request:ok"){  
+						            	console.log('ok')
 						                alert("微信支付成功!");  
 						            }else if(res.err_msg == "get_brand_wcpay_request:cancel"){  
 						                alert("用户取消支付!");  
