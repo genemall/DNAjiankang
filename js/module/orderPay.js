da6c68ed7d1ac14f6c16a99788be132a0e0dc1ce
shileiding <shileiding@163.com>
2016-12-25 19:34:28 +0800

    测试 获取用户地址

--- js/module/orderPay.js
+++ js/module/orderPay.js
@@ -25,13 +25,48 @@ angular.module('orderPay', ['ui.router'])
                              .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
                                 $scope.pay_data=data
                         });
-                        
+                        //获取 address 配置
                         httpService.post($rootScope.baseURL+'weixin/addresstest.do',{})
-                             .then(function (data) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
-                                console.log(data)
+                             .then(function (msg) {//.then()函数里的返回值解析.这适用于对返回值做一些处理后再返回.
+                                wx.config({
+						            debug: false,
+						            appId: msg.appid,
+						            timestamp: msg.timestamp,
+						            nonceStr: msg.noncestr,
+						            signature: msg.signature,
+						            jsApiList: [
+						              // 所有要调用的 API 都要加到这个列表中
+						                'checkJsApi',
+						                'getUserAddress',
+						                
+						              ]
+						          });
+						        wx.checkJsApi({
+						    	      jsApiList: [
+						    	          'getUserAddress',
+						    	      ],
+						    	      success: function (res) {
+						    	          alert(res);
+						    	      }
+						    	});   
                         });
+                        //执行获取用户地址
                         $scope.getUserAddress=function(){
-                        	
+                        	wx.openAddress({
+					            trigger: function (res) {
+					              alert('用户开始拉出地址');
+					            },
+					            success: function (res) {
+					              alert('用户成功拉出地址');
+					              alert(JSON.stringify(res));
+					            },
+					            cancel: function (res) {
+					              alert('用户取消拉出地址');
+					            },
+					            fail: function (res) {
+					              alert(JSON.stringify(res));
+					            }
+					          });
                         }
                     	$scope.gotoPay=function(){
                             console.log($scope.pay_data)
