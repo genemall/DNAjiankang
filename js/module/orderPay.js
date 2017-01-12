b4d7fd2ca54118b92a19cff74d7e15f06e86de41
15258230407@163.com <shileiding@pptv.com>
2017-01-12 16:37:14 +0800

    分享

--- js/module/orderPay.js
+++ js/module/orderPay.js
@@ -64,7 +64,20 @@ angular.module('orderPay', ['ui.router','utilMd'])
 					    	          //alert(JSON.stringify(res));
 					    	      }
 							}); 
-							httpService.share_link("product_details.proName","product_details.proHead","product_details.imagelist[0].url")
+							  	wx.onMenuShareAppMessage({
+							   	    title: "_title", // 分享标题
+								    desc: "_desc", // 分享描述
+								    link: '', // 分享链接
+								    imgUrl: "_image", // 分享图标
+								    type: '', // 分享类型,music、video或link，不填默认为link
+								    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+								    success: function () { 
+								        // 用户确认分享后执行的回调函数
+								    },
+								    cancel: function () { 
+								        // 用户取消分享后执行的回调函数
+								    }
+								});
 							//wx.hideAllNonBaseMenuItem();
                     	}
                         //根据cookie判断地址是否配置和加载
